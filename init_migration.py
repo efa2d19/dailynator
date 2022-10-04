b05b9e47503eb497a8ce9fad2d381ac5a8713c6f
Drugsosos <44712637+Drugsosos@users.noreply.github.com>
2022-10-05 04:43:54 +0600

    updated initial migration for new db queries

--- init_migration.py
+++ init_migration.py
@@ -10,7 +10,16 @@ def init_migration():
     cursor.execute("PRAGMA foreign_keys = ON")
 
     # Create channels table
-    cursor.execute("create table channels (channel_id TEXT PRIMARY KEY NOT NULL, cron TEXT)")
+    cursor.execute(
+        """\
+create table channels (
+    channel_id TEXT PRIMARY KEY NOT NULL,
+    team_id TEXT NOT NULL,
+    channel_name TEXT NOT NULL,
+    cron TEXT
+)\
+"""
+    )
 
     # Create questions table
     cursor.execute("create table questions (body TEXT NOT NULL)")
@@ -23,6 +32,7 @@ create table users (
     daily_status INTEGER NOT NULL,
     q_idx INTEGER,
     main_channel_id TEXT NOT NULL,
+    real_name TEXT NOT NULL,
     FOREIGN KEY (main_channel_id) REFERENCES channels (channel_id)
 )\
 """
