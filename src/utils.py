5a846eb3621ce80244542e1de33bea43b4bf8317
Drugsosos <44712637+Drugsosos@users.noreply.github.com>
2022-10-05 17:29:49 +0600

    added skip_cron & improved start_cron

--- src/utils.py
+++ src/utils.py
@@ -20,10 +20,14 @@ async def parse_emoji_list(
     return list(emoji_r.data.get("emoji", {':+1:'}).keys())
 
 
-async def start_cron():
+async def start_cron() -> None:
+    """
+    Adds cron triggered jobs to default async job store
+    """
+
     from functools import partial
     from apscheduler.triggers.cron import CronTrigger
-    from main import scheduler
+    from main import scheduler, app
     from src.report import start_daily
     from src.db import Database
 
@@ -32,32 +36,99 @@ async def start_cron():
     # Get current cron as str
     cron_list = await db.get_all_cron_with_channels()
 
-    # Remove all existing triggers
-    scheduler.remove_all_jobs()
-
     for channel_id, team_id, cron in cron_list:
         # Skip if cron not set
         if not cron:
+            from src.block_kit import error_block
+
+            await app.client.chat_postMessage(
+                channel=channel_id,
+                text=":x: No scheduler was added",
+                blocks=error_block(
+                    header_text="No scheduler was added",
+                    body_text="Can't start daily 'cause no scheduler was added.\n" +
+                              "Use this to add schedule `/cron <* * * * *>`",
+                ),
+            )
             return
 
-        # Split cron
-        split_cron = cron.split(" ")
-
-        # Filter stars
-        split_cron = [unit if unit != "*" else None for unit in split_cron]
-
-        # Collect kwargs for CronTrigger
-        kwargs = dict(zip(['minute', 'hour', 'day', 'month', 'day_of_week'], split_cron))
-
         # Get an instance of CronTrigger
-        cron_trigger = CronTrigger(**kwargs)
+        cron_trigger = CronTrigger().from_crontab(cron)
 
         # Schedule a job
         scheduler.add_job(
             func=partial(start_daily, channel_id=channel_id),  # Supply channel_id to start_daily
             trigger=cron_trigger,
-            name=f"start_daily_{team_id}_{channel_id}"
+            id=f"{team_id}_{channel_id}",
+            replace_existing=True,
         )
+
+    # Start Async scheduler
+    if not scheduler.state:
+        scheduler.start()
+
+
+async def skip_cron(
+        channel_id: str,
+) -> None:
+    """
+    Skip next daily trigger time for specified channel
+
+    :param channel_id: Slack channel id
+    """
+
+    from functools import partial
+    from apscheduler.triggers.cron import CronTrigger
+    from apscheduler.job import Job
+    from main import scheduler, app
+    from src.report import start_daily
+    from src.db import Database
+
+    db = Database()
+
+    # Get channel cron and team_id
+    channel_cron, team_id = await db.get_cron_by_channel_id(
+        channel_id=channel_id,
+    )
+
+    # Send notification to channel if there is no cron or team id
+    if not channel_cron or not team_id:
+        from src.block_kit import error_block
+
+        await app.client.chat_postMessage(
+            channel=channel_id,
+            text=":x: Can't skip scheduled daily",
+            blocks=error_block(
+                header_text="Can't skip scheduled daily",
+                body_text="Schedule wasn't set or channel wasn't parsed properly",
+            ),
+        )
+
+    from datetime import timedelta
+
+    # Get previous job's trigger
+    cron_job: Job = scheduler.get_job(
+        job_id=f"{team_id}_{channel_id}",
+    )
+
+    cron_trigger: CronTrigger = cron_job.trigger
+
+    # Get previous job's next fire datetime
+    cron_trigger_next_fire_time = cron_trigger.get_next_fire_time(
+        previous_fire_time=cron_job.next_run_time,  # noqa
+        now=cron_job.next_run_time + timedelta(seconds=1),
+    )
+
+    # Set new start time
+    cron_trigger.start_date = cron_trigger_next_fire_time
+
+    # Update a job w/ trigger w/ new start time
+    scheduler.add_job(
+        func=partial(start_daily, channel_id=channel_id),  # Supply channel_id to start_daily
+        trigger=cron_trigger,
+        id=f"{team_id}_{channel_id}",
+        replace_existing=True,
+    )
 
     # Start Async scheduler
     if not scheduler.state:
@@ -85,11 +156,12 @@ async def is_dm_in_command(
         from src.block_kit import error_block
 
         await client.chat_postEphemeral(
+            channel=channel_id,
+            text=":x: You can't use command in DMs",
             blocks=error_block(
                 header_text="You can't use command in DMs",
                 body_text="Open any available channel and send the command there",
             ),
-            channel=channel_id,
             user=user_id,
         )
         return True
