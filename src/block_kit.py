f67c3cafe7ebd1094912a1910ca7c1c82f541a48
Drugsosos <44712637+Drugsosos@users.noreply.github.com>
2022-10-06 23:55:14 +0600

    added docs in block_kit

--- src/block_kit.py
+++ src/block_kit.py
@@ -15,6 +15,15 @@ def report_attachment_block(
         body_text: str,
         color: str,
 ) -> BlockAttachment:
+    """
+    Attachment unit to be sent as a report in the channel
+
+    :param header_text: Question text (no markdown)
+    :param body_text: Answer text (has markdown)
+    :param color: Color of the strip on the left side of the block
+    :return: Report block unit
+    """
+
     return BlockAttachment(
         blocks=[
             HeaderBlock(
@@ -35,6 +44,15 @@ def start_daily_block(
         body_text: str,
         first_question: str,
 ) -> Sequence[Block]:
+    """
+    Set of blocks to be sent to users on daily start
+
+    :param header_text: Greetings above divider
+    :param body_text: Something inspirational (yup, daily is hard, cheer 'em up)
+    :param first_question: First question from question list
+    :return: Blocks to be sent on daily start
+    """
+
     return [
         ContextBlock(
             elements=[
@@ -62,6 +80,15 @@ def end_daily_block(
         end_body_text: str,
         footer_text: str,
 ) -> Sequence[Block]:
+    """
+    Set of blocks to be sent on daily end (all questions ended)
+
+    :param start_body_text: Show 'em your gratitude here and tag 'em
+    :param end_body_text: Something inspirational again (they have all day ahead)
+    :param footer_text: Where to find a report (add chanel link here)
+    :return: Blocks to be sent on daily end
+    """
+
     return [
         SectionBlock(
             text=MarkdownTextObject(
@@ -87,8 +114,16 @@ def end_daily_block(
 def question_list_block(
         question_list: list[str, str],
 ) -> Sequence[Block]:
+    """
+    Blocks constructor for get questions command
+
+    :param question_list: List of all question for the channel
+    :return: List of blocks w/ questions
+    """
+
     field_list = list()
 
+    # Add indexes (for pop command)
     for idx, question in enumerate(question_list, start=1):
         field_list.append(
             MarkdownTextObject(
@@ -111,6 +146,14 @@ def error_block(
         header_text: str,
         body_text: str,
 ) -> Sequence[Block]:
+    """
+    Make it look beautiful at least when error occurs
+
+    :param header_text: Error summary
+    :param body_text: Error main message
+    :return: Error block
+    """
+
     return [
         HeaderBlock(
             text=":x:\t" + header_text,
@@ -130,6 +173,14 @@ def success_block(
         header_text: str,
         body_text: Optional[str] = None,
 ) -> Sequence[Block]:
+    """
+    Block to be shown on success execution
+
+    :param header_text: Success summary
+    :param body_text: Success main message (optional)
+    :return: Success block
+    """
+
     blocks = [
         HeaderBlock(
             text=":white_check_mark:\t" + header_text,
